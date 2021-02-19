9e20b3db2adbf6850faf6c5015a51a7b03c8e4fa
Emojigit <55009343+Emojigit@users.noreply.github.com>
2021-02-19 17:52:36 +0800

    count all year that pandas support

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import datetime, pytz, os, time, pandas
 os.environ['TZ'] = 'UTC'
 time.tzset()
 # 參考：Emojiwiki（公元28年1月－224年4月）
-fyear = datetime.datetime(2019,7,3,0,0)# 2019年7月3日上午12：00
-nyear = datetime.datetime.today() #Now date
-#nyear = datetime.datetime(2022,4,10,0,0)
+fyear = datetime.datetime(2019,7,4,12,0)# 2019年7月4日下午12：00
+#nyear = datetime.datetime.today() #Now date
+nyear = pandas.Timestamp.max
 oyear = datetime.timedelta(hours=12)
 print("start:",fyear)
 #print("2:",nyear)
